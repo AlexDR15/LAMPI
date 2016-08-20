f284cebe3657cf8968d4c4a406cb7b562fd6b08f
AlexDR15 <AlexDR15@users.noreply.github.com>
2016-08-20 12:02:45 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>LAMPI - An Automatic LAMP Auto Installer</title>
+</head>
+<body>
+	<h1>LAMPI</h1>
+	<h2>Coming Soon...</h2>
+</body>
+</html>
